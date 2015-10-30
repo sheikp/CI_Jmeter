2010302fcccc2412d49f497e2ad0454f414f0385
jackchem2003 <jackchem2003@gmail.com>
2015-10-30 03:49:45 -0700

    test

--- firsttest/src/firsttest/DatabaseTesingDemo.java
+++ firsttest/src/firsttest/DatabaseTesingDemo.java
@@ -11,7 +11,7 @@ public class DatabaseTesingDemo
 {
 // Connection object
 	       static Connection con = null;
-	       //testing
+	       //test
 // Statement object
 	       private static Statement stmt;
 	       // Constant for Database URL
